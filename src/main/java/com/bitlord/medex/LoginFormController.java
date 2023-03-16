4d0421cb10e4fb31b04be4165fac7730a33798ef
Malinga Bandara <malinga1021@gmail.com>
2023-03-16 22:41:11 +0530

    Login Validation b1

--- src/main/java/com/bitlord/medex/LoginFormController.java
+++ src/main/java/com/bitlord/medex/LoginFormController.java
@@ -1,15 +1,19 @@
 package com.bitlord.medex;
 
+import com.bitlord.medex.db.Database;
+import com.bitlord.medex.dto.UserDto;
 import com.jfoenix.controls.JFXRadioButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Locale;
 
 public class LoginFormController {
     public JFXTextField txtEmail;
@@ -19,6 +23,34 @@ public class LoginFormController {
     public AnchorPane logingContext;
 
     public void signinOnAction(ActionEvent actionEvent) {
+
+        // get userName and password
+        String email = txtEmail.getText();
+        String password = txtPassword.getText();
+
+            // find user in database
+             for ( UserDto dto :Database.userTable ) {
+
+                 if ( dto.getEmail().trim().toLowerCase().equals( email ) ) { // if email available
+
+                     if ( dto.getPassword().equals( password ) ) { // if password correct
+
+                         // check Account Type
+
+                     }else {                                       // the password incorrect then
+                         new Alert( Alert.AlertType.WARNING, "Your Password is incorrect !" ).show();
+                         return;
+                     }
+
+                 }
+
+             }
+
+
+        // get the correct user
+
+        // check his account type
+
 
     }
 
