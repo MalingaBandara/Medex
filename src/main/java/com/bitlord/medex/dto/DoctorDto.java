92936897303ed55eb866525ebca1e96fe6c3f3a6
Malinga Bandara <malinga1021@gmail.com>
2023-03-18 19:45:36 +0530

    setup deafult doctor to dto #4

---
+++ src/main/java/com/bitlord/medex/dto/DoctorDto.java
@@ -0,0 +1,16 @@
+package com.bitlord.medex.dto;
+
+import com.bitlord.medex.enums.GenderType;
+
+public class DoctorDto {
+
+    String firstName;
+    String lastName;
+    String nic;
+    String contact;
+    String email;
+    String specializations;
+    String Address;
+    GenderType gender;
+
+}
---
+++ src/main/java/com/bitlord/medex/enums/GenderType.java
@@ -0,0 +1,13 @@
+package com.bitlord.medex.enums;
+
+public enum GenderType {
+
+    MALE(1) , Fe_MALE(2);
+
+    final int state;
+
+    GenderType(int state) {
+        this.state = state;
+    }
+
+}
