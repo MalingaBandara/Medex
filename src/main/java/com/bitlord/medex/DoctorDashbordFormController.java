69e8a18a00652c547be49c6486a6a3e6b90f16e4
Malinga Bandara <malinga1021@gmail.com>
2023-03-18 11:25:00 +0530

    time setting in doctor dashboard #4

--- src/main/java/com/bitlord/medex/DoctorDashbordFormController.java
+++ src/main/java/com/bitlord/medex/DoctorDashbordFormController.java
@@ -1,14 +1,20 @@
 package com.bitlord.medex;
 
 import com.bitlord.medex.util.Cookie;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class DoctorDashbordFormController {
@@ -30,6 +36,21 @@ public class DoctorDashbordFormController {
         String today  = simpleDateFormat.format(date);
         lblDate.setText( today );*/
         lblDate.setText( new SimpleDateFormat( "yyyy-MM-dd" ).format( new Date() ) ); // set date to the date label
+
+        Timeline timeline = new Timeline(  // timeline animation object
+                    new KeyFrame ( Duration.seconds(0), // create new keyframe object
+
+                                    e -> {
+                                        DateTimeFormatter dtf = DateTimeFormatter.ofPattern( "hh:mm:ss" ); // set time format
+                                        lblTime.setText( LocalTime.now().format( dtf ) ); // set local time to label with format custom format of dtf
+                                    }
+
+                                )
+                            ,
+                            new KeyFrame( Duration.seconds( 1) ) // change time animation by one second
+                );
+        timeline.setCycleCount( Animation.INDEFINITE );
+        timeline.play(); // start timeline
 
     }
 
