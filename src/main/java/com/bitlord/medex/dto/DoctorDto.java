24fb859519a6d1b123e81b79647011e00d695bb3
Malinga Bandara <malinga1021@gmail.com>
2023-03-18 19:46:58 +0530

    setup deafult doctor to dto #4

--- src/main/java/com/bitlord/medex/dto/DoctorDto.java
+++ src/main/java/com/bitlord/medex/dto/DoctorDto.java
@@ -11,6 +11,75 @@ public class DoctorDto {
     String email;
     String specializations;
     String Address;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSpecializations() {
+        return specializations;
+    }
+
+    public void setSpecializations(String specializations) {
+        this.specializations = specializations;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public GenderType getGender() {
+        return gender;
+    }
+
+    public void setGender(GenderType gender) {
+        this.gender = gender;
+    }
+
     GenderType gender;
+
+
+
+
 
 }
