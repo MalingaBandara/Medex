f8de18206499a5a2f74d51678ac3765230db5281
Malinga Bandara <malinga1021@gmail.com>
2023-03-19 17:51:26 +0530

    set a value to cookie for use current development #4

--- src/main/java/com/bitlord/medex/util/Cookie.java
+++ src/main/java/com/bitlord/medex/util/Cookie.java
@@ -1,9 +1,10 @@
 package com.bitlord.medex.util;
 
+import com.bitlord.medex.db.Database;
 import com.bitlord.medex.dto.UserDto;
 
 public class Cookie {
 
-    public static UserDto selectedUser;  // default value ---> null
+    public static UserDto selectedUser = Database.userTable.get( 1 );  // default value ---> null
 
 }
