22f5e9359e604fe8f071db16c6a8e065dd3fd4a6
Malinga Bandara <malinga1021@gmail.com>
2023-03-23 03:08:28 +0530

    Add CRUD util java class for executeQuery() and  executeUpdate() (Modern Way)

--- src/main/java/com/bitlord/medex/util/CrudUtil.java
+++ src/main/java/com/bitlord/medex/util/CrudUtil.java
@@ -3,14 +3,14 @@ package com.bitlord.medex.util;
 import com.bitlord.medex.db.DBConnection;
 
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CrudUtil {
 
-    //  ways =>  , modern
+    //  ways =>   , modern
 
-    private static PreparedStatement execute ( String sql, Object...params ) throws SQLException, ClassNotFoundException {
+    // Legacy Way
+        /*    private static PreparedStatement execute ( String sql, Object...params ) throws SQLException, ClassNotFoundException {
 
         PreparedStatement pstm = DBConnection.getInstance().getConnection().prepareStatement(sql);
 
@@ -41,7 +41,26 @@ public class CrudUtil {
 
         return execute( sql, params ).executeQuery();
 
-    }
+    }*/
+
+    // Modern Way
+        private static <T> T execute ( String sql, Object...params ) throws SQLException, ClassNotFoundException {
+
+                PreparedStatement pstm = DBConnection.getInstance().getConnection().prepareStatement(sql);
+
+                for ( int i = 0; i < params.length; i++ ) {
+
+                    pstm.setObject( ( i + 1 ), params[ i ] );
+
+                }
+
+                if ( sql.startsWith( "SELECT" ) ) {
+                    return (T)pstm.executeQuery();
+                }
+
+                return  (T)(Boolean) ( pstm.executeUpdate()>0 );
+
+        }
 
 
 }
