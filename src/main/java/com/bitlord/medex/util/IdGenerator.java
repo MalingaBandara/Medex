b5fa9195cd90484f70a50d897d3308e52a432bf9
Malinga Bandara <malinga1021@gmail.com>
2023-03-22 22:13:35 +0530

    generate auto id

---
+++ src/main/java/com/bitlord/medex/util/IdGenerator.java
@@ -0,0 +1,42 @@
+package com.bitlord.medex.util;
+
+import java.sql.*;
+
+public class IdGenerator {
+
+    public int generateId () {
+
+        try {
+
+                //  1 driver Load => dependency
+                Class.forName( "com.mysql.cj.jdbc.Driver" );
+
+                //  2 Create a Connection
+                Connection connection = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/medex",
+                        "root",
+                        "spymali1021"
+                );
+
+            // 3 write a SQL
+            String sql = "SELECT id FROM user ORDER BY user_id DESC LIMIT 1";
+
+            PreparedStatement pstm = connection.prepareStatement(sql);
+
+            // CRUD ==> Get(Read) => executeQuery() ===> Data set
+            // CRUD ==> save(create), delete , update  => executeUpdate()
+            ResultSet rst = pstm.executeQuery();
+
+                if ( rst.next() ) {
+                    return rst.getInt(1 ) + 1;
+                }
+
+
+        } catch (ClassNotFoundException | SQLException e ) {
+            e.printStackTrace();
+        }
+        return 1;
+
+    }
+
+}
