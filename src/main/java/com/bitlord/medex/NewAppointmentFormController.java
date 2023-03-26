c2f637534922d54626cdcd9fb76bba4b5128733c
Malinga Bandara <malinga1021@gmail.com>
2023-03-27 00:49:54 +0530

    Add new appointment Form

--- src/main/java/com/bitlord/medex/NewAppointmentFormController.java
+++ src/main/java/com/bitlord/medex/NewAppointmentFormController.java
@@ -1,4 +1,26 @@
 package com.bitlord.medex;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class NewAppointmentFormController {
+    public JFXTextField txtDate;
+    public AnchorPane newAppoinmentContext;
+    public JFXComboBox cmbDoctor;
+    public JFXTextField txtTime;
+    public JFXTextField txtAmount;
+    public JFXTextArea txtMessage;
+
+    public void backToHmeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void seeAvailabilityOnAction(ActionEvent actionEvent) {
+    }
+
+    public void submitDataOnAction(ActionEvent actionEvent) {
+
+    }
 }
