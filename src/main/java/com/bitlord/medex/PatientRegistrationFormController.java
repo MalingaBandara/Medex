7587bc99dbaa1a32e084ccebfaff8b59f2eb5f88
Malinga Bandara <malinga1021@gmail.com>
2023-03-26 23:20:45 +0530

    Add patient Registration Form save data

--- src/main/java/com/bitlord/medex/PatientRegistrationFormController.java
+++ src/main/java/com/bitlord/medex/PatientRegistrationFormController.java
@@ -1,15 +1,17 @@
 package com.bitlord.medex;
 
 import com.bitlord.medex.dto.User;
+import com.bitlord.medex.enums.GenderType;
 import com.bitlord.medex.util.Cookie;
 import com.bitlord.medex.util.CrudUtil;
-import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXRadioButton;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -27,8 +29,9 @@ public class PatientRegistrationFormController {
     public JFXTextField txtEmail;
     public JFXTextArea txtAddress;
     public ToggleGroup gender;
-    public JFXDatePicker txtDob;
+   
     public JFXRadioButton rBtnmale;
+    public DatePicker txtDob;
 
 
     public void initialize() {
@@ -67,8 +70,37 @@ public class PatientRegistrationFormController {
 
     public void submitDataOnAtion(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
 
+        try {
+
+            String patientId = generatePatientId();
+
+            // insert values
+            boolean isSave =  CrudUtil.execute( "INSERT INTO patient VALUES (?,?,?,?,?,?,?,?,?)" ,
+
+                    patientId,
+                    txtFirstName.getText(),
+                    txtLastName.getText(),
+                    txtEmail.getText(),
+                    txtContact.getText(),
+                    txtNic.getText(),
+                    txtAddress.getText(),
+                    txtDob.getValue(),
+                    rBtnmale.isSelected() ? GenderType.MALE.name() : GenderType.FE_MALE.name()
+
+            );
+
+            if ( isSave ) {
+                new Alert( Alert.AlertType.INFORMATION, "Welcome Patient..." ).show();
+                setUI("PatientDashboardForm");
+            }
+
+
+        } catch ( SQLException | ClassNotFoundException | IOException e ) {
+            e.printStackTrace();
+        }
+
     }
-//
+
 
     // method for redirect forms
     private void setUI ( String location ) throws IOException {
