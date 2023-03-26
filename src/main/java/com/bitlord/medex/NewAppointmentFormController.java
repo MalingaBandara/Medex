0e88512d5d8a774296812ea5eac168455a3eb832
Malinga Bandara <malinga1021@gmail.com>
2023-03-27 01:33:51 +0530

    Load new appointment Form and doctors ids

--- src/main/java/com/bitlord/medex/NewAppointmentFormController.java
+++ src/main/java/com/bitlord/medex/NewAppointmentFormController.java
@@ -1,26 +1,58 @@
 package com.bitlord.medex;
 
+import com.bitlord.medex.util.CrudUtil;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.layout.AnchorPane;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class NewAppointmentFormController {
     public JFXTextField txtDate;
     public AnchorPane newAppoinmentContext;
-    public JFXComboBox cmbDoctor;
+    public JFXComboBox<String> cmbDoctor;
     public JFXTextField txtTime;
     public JFXTextField txtAmount;
     public JFXTextArea txtMessage;
 
-    public void backToHmeOnAction(ActionEvent actionEvent) {
+    public void initialie() {
+        seDoctorIds();
     }
+
+    private void seDoctorIds() {
+
+        try {
+
+                ResultSet set = CrudUtil.execute("SELECT doctor_id,first_name,last_name FROM doctor"); // get doctors data
+
+                    ObservableList<String> obList = FXCollections.observableArrayList(); // list to add recodes
+
+                        while (set.next() ) {
+                                obList.add(set.getString( 1) );
+                        }
+
+                cmbDoctor.setItems( obList );
+
+        } catch ( SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 
     public void seeAvailabilityOnAction(ActionEvent actionEvent) {
     }
 
     public void submitDataOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void backToHmeOnAction(ActionEvent actionEvent) {
 
     }
 }
--- src/main/java/com/bitlord/medex/PatientDashboardFormController.java
+++ src/main/java/com/bitlord/medex/PatientDashboardFormController.java
@@ -1,8 +1,13 @@
 package com.bitlord.medex;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class PatientDashboardFormController {
 
@@ -11,6 +16,20 @@ public class PatientDashboardFormController {
     public Label lblDate;
     public Label lblTime;
 
-    public void navigateToNewAppointmentPage(ActionEvent actionEvent) {
+    public void navigateToNewAppointmentPage(ActionEvent actionEvent) throws IOException {
+
+        setUI( "NewAppointmentForm" );
+
     }
+
+    // method for redirect forms
+    private void setUI ( String location ) throws IOException {
+
+        Stage stage = (Stage) patientDashboardContext.getScene().getWindow();
+
+        stage.setScene( new Scene( FXMLLoader.load( getClass().getResource("/com/bitlord/medex/"+ location +".fxml"))));
+
+    }
+
+
 }
